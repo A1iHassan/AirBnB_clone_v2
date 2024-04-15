05943bbe743d33b2ca12326ca78c47504c6b4f50
A1iHassan <ali012wkout@gmail.com>
2024-04-15 12:40:31 +0200

    commit by Ali Hassan

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
--- creating the database hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creating the database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- creating user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
@@ -11,3 +11,15 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- granting select privilege on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- creating the database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- creating user hbnb_test
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- granting all privileges on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- granting select privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
