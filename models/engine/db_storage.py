42d13c945f47c1cafc50915de15dd45963d8f8c8
A1iHassan <ali012wkout@gmail.com>
2024-04-17 18:37:40 +0200

    commit by Ali Hassan

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,11 +4,13 @@ a module for the db engine storage
 """
 from sqlalchemy import create_engine
 from os import environ as env
+from models.base_model import Base
 
 user = env['HBNB_MYSQL_USER']
 pwd = env['HBNB_MYSQL_PWD']
 host = env['HBNB_MYSQL_HOST']
 db = env['HBNB_MYSQL_DB']
+Env = env['HBNB_ENV']
 
 
 class DBStorage:
@@ -24,3 +26,6 @@ class DBStorage:
 		instantiates new instances
 		"""
 		self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}', pool_pre_ping=True)
+		if Env == 'test':
+			Base.metadata.reflect(self.__engine)
+			Base.metadata.drop_all(self.__engine)
